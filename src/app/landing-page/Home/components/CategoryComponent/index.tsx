8de330c2f70bd1ff930a17d96ce96c9d4739845a
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2025-11-20 17:31:02 -0300

    adicionando imagens e arquivo de produtos

--- src/app/landing-page/Home/components/CategoryComponent/index.tsx
+++ src/app/landing-page/Home/components/CategoryComponent/index.tsx
@@ -1,89 +1,110 @@
-"use client"
-import * as React from "react"
-import { Card, CardContent, CardTitle } from "@/components/ui/card"
-import { CardSimIcon, SaladIcon } from "lucide-react"
-import "./styles.min.scss"
+"use client";
+import * as React from "react";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { CardSimIcon, SaladIcon } from "lucide-react";
+import "./styles.min.scss";
+import clsx from "clsx";
 
 export type CategoryProps = {
-    icon: React.ElementType
-    name: string
-}
+  icon: React.ElementType;
+  name: string;
+};
 
 export default function CategoryComponent() {
-    const categoryList: CategoryProps[] = [
-        {
-            icon: CardSimIcon,
-            name: "card1"
-        },
-        {
-            icon: SaladIcon,
-            name: "card2"
-        },
-        {
-            icon: CardSimIcon,
-            name: "card13"
-        },
-        {
-            icon: SaladIcon,
-            name: "card233"
-        },
-        {
-            icon: CardSimIcon,
-            name: "card1333"
-        },
-        {
-            icon: SaladIcon,
-            name: "card23333"
-        },
-        {
-            icon: CardSimIcon,
-            name: "card133333"
-        },
-        {
-            icon: SaladIcon,
-            name: "card23333333"
-        },
-        {
-            icon: SaladIcon,
-            name: "card22222"
-        },
-        {
-            icon: CardSimIcon,
-            name: "card22221"
-        },
-        {
-            icon: SaladIcon,
-            name: "card2222233"
-        },
-        {
-            icon: CardSimIcon,
-            name: "card12223342"
-        },
-        {
-            icon: SaladIcon,
-            name: "card2777"
-        },
-    ]
+  const categoryList: CategoryProps[] = [
+    {
+      icon: CardSimIcon,
+      name: "card1",
+    },
+    {
+      icon: SaladIcon,
+      name: "card2",
+    },
+    {
+      icon: CardSimIcon,
+      name: "card13",
+    },
+    {
+      icon: SaladIcon,
+      name: "card233",
+    },
+    {
+      icon: CardSimIcon,
+      name: "card1333",
+    },
+    {
+      icon: SaladIcon,
+      name: "card23333",
+    },
+    {
+      icon: CardSimIcon,
+      name: "card133333",
+    },
+    {
+      icon: SaladIcon,
+      name: "card23333333",
+    },
+    {
+      icon: SaladIcon,
+      name: "card22222",
+    },
+    {
+      icon: CardSimIcon,
+      name: "card22221",
+    },
+    {
+      icon: SaladIcon,
+      name: "card2222233",
+    },
+    {
+      icon: CardSimIcon,
+      name: "card12223342",
+    },
+    {
+      icon: SaladIcon,
+      name: "card2777",
+    },
+  ];
 
-    return (
-        <>
-            <Card className="w-full border-transparent shadow-transparent mt-2 mx-auto min-w-[300px] sm:max-w-sm md:max-w-md lg:max-w-4xl">
-                <CardContent className="flex px-2 flex-col gap-6">
-                    <CardTitle>
-                        Navegue por categorias:
-                    </CardTitle>
-                    <div className="card_category overflow-x-scroll overflow-y-hidden flex gap-2">
-                        {
-                            categoryList.map((category) => (
-                                <Card className="p-2 w-full shadow-accent bg-blue-100 hover:bg-blue-300 hover:shadow-blue-900/30 shadow-lg ease-in-out  cursor-pointer rounded-full flex justify-center items-center" key={category.name}>
-                                    <category.icon className="btn_category w-10 h-10" />
-                                </Card>
-                            ))
-                        }
-                    </div>
-
-                </CardContent>
-            </Card>
-        </>
-    )
+  return (
+    <>
+      <Card className="w-full border-transparent shadow-transparent my-2 mx-auto min-w-[300px] sm:max-w-sm md:max-w-md lg:max-w-4xl">
+        <CardContent className="flex px-2 flex-col gap-6">
+          <CardTitle>Navegue por categorias:</CardTitle>
+          <div
+            className={clsx(
+              "card_category",
+              "overflow-x-scroll",
+              "overflow-y-hidden",
+              "flex",
+              "gap-2"
+            )}
+          >
+            {categoryList.map((category) => (
+              <Card
+                className={clsx(
+                  "p-2",
+                  "w-full",
+                  "shadow-accent",
+                  "bg-blue-100",
+                  "hover:bg-blue-300",
+                  "hover:shadow-blue-900/30",
+                  "shadow-lg",
+                  "ease-in-out",
+                  "cursor-pointer",
+                  "rounded-full",
+                  "flex",
+                  "justify-center",
+                  "items-center"
+                )}
+                key={category.name}
+              >
+                <category.icon className={clsx('btn_category', 'w-10', 'h-10')} />
+              </Card>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
--- src/app/landing-page/Home/page.tsx
+++ src/app/landing-page/Home/page.tsx
@@ -1,22 +1,50 @@
-import { Card, CardTitle } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { CarouselComponent } from "./components/CarouselComponent";
 import CategoryComponent from "./components/CategoryComponent";
+import clsx from "clsx";
+import Image from "next/image";
 
 function HomePage() {
-    return (
-        <>
-            <CarouselComponent />
-            <CategoryComponent />
-            <Card className="w-full mx-auto min-w-[300px] sm:max-w-sm md:max-w-md lg:max-w-4xl px-2">
-                <CardTitle>Produtos em promocção</CardTitle>
-                <Card className="grid grid-cols-2 gap-2 px-2 md:grid-cols-3 lg:grid-cols-4 shadow-transparent border-transparent">
-                    <Card className="min-h-80"></Card>
-                    <Card className="min-h-80"></Card>
-                    <Card className="min-h-80"></Card>
-                    <Card className="min-h-80"></Card>
-                </Card>
-            </Card>
-        </>
-    )
+  return (
+    <>
+      <CarouselComponent />
+      <CategoryComponent />
+      <Card
+        className={clsx(
+          "w-full",
+          "mx-auto",
+          "min-w-[300px]",
+          "sm:max-w-sm",
+          "md:max-w-md",
+          "lg:max-w-4xl",
+          "px-2"
+        )}
+      >
+        <CardTitle>Produtos em promocção</CardTitle>
+        <Card
+          className={clsx(
+            "grid",
+            "grid-cols-2",
+            "gap-2",
+            "px-2",
+            "md:grid-cols-3",
+            "lg:grid-cols-4",
+            "shadow-transparent",
+            "border-transparent",
+            "px-0"
+          )}
+        >
+          <Card className="min-h-80">
+            <CardHeader className="relative">
+                <Image className={clsx("w-7 absolute left-1 -top-4 bg-gray-900/20 rounded-full flex justify-center items-center p-0.5")} src={"/fire.gif"} width={1080} height={720} alt="fire" />
+            </CardHeader>
+          </Card>
+          <Card className="min-h-80"></Card>
+          <Card className="min-h-80"></Card>
+          <Card className="min-h-80"></Card>
+        </Card>
+      </Card>
+    </>
+  );
 }
 export default HomePage;
