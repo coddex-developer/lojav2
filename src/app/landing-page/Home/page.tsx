1f316f171c321daa63b8a4b6194c70195c7ce968
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2025-11-20 16:22:22 -0300

    Adicionando container vitrine de produtos

--- src/app/landing-page/Home/page.tsx
+++ src/app/landing-page/Home/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardTitle } from "@/components/ui/card";
 import { CarouselComponent } from "./components/CarouselComponent";
 import CategoryComponent from "./components/CategoryComponent";
 
@@ -6,6 +7,15 @@ function HomePage() {
         <>
             <CarouselComponent />
             <CategoryComponent />
+            <Card className="w-full mx-auto min-w-[300px] sm:max-w-sm md:max-w-md lg:max-w-4xl px-2">
+                <CardTitle>Produtos em promocção</CardTitle>
+                <Card className="grid grid-cols-2 gap-2 px-2 md:grid-cols-3 lg:grid-cols-4 shadow-transparent border-transparent">
+                    <Card className="min-h-80"></Card>
+                    <Card className="min-h-80"></Card>
+                    <Card className="min-h-80"></Card>
+                    <Card className="min-h-80"></Card>
+                </Card>
+            </Card>
         </>
     )
 }
